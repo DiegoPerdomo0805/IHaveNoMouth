0e58ff6bea66dcb0844a65abf678f41b11f1e9ff
dyegoblackk1406 <diegocervantescc@gmail.com>
2025-08-30 02:00:31 -0600

    SINTETIZADOR Y BATERÍA TERMINADOS

--- PatronSintetizador.py
+++ PatronSintetizador.py
@@ -1,12 +1,11 @@
 # Sintetizador
 
-# SECCIÓN AÚN NO TERMINADA
-
 # SE INCLUYE SECCIÓN DE PERCUSIÓN PARA ACOMPAÑAR MIENTRAS SE COMPONE
 
 from music import *
  
 repetitions = 4     
+
  
 ##### define the data structure
 score = Score("Rhythm Section Pattern", 90.0)
@@ -24,37 +23,33 @@ bassPhrase = Phrase(0.0)
 ##### create musical data
  
 # bass drum pattern
-bassPitches   = [BDR, BDR, BDR, BDR] * 32          # El bombo nunca cambia
-bassDurations = [QN, QN, QN, QN] * 32
+bassPitches   = [BDR, BDR, BDR, BDR] * 8          # El bombo nunca cambia
+bassDurations = [QN, QN, QN, QN] * 8
 bassDrumPhrase.addNoteList(bassPitches, bassDurations)
 
-# hi-hat pattern 2                           # Patrón de hihat para coro
-#hiHatPitchesIntro   = [CHH, CHH, CHH, OHH, CHH, CHH, OHH, CHH, CHH, OHH, CHH, OHH, CHH, CHH, OHH, CHH] * 4
-#hiHatDurationsIntro = [SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN] * 4
-#hiHatPhrase.addNoteList(hiHatPitchesIntro, hiHatDurationsIntro)
+ #hi-hat pattern 2                           # Patrón de hihat para coro
+hiHatPitchesIntro   = [CHH, CHH, CHH, OHH, CHH, CHH, OHH, CHH, CHH, OHH, CHH, OHH, CHH, CHH, OHH, CHH] * 4
+hiHatDurationsIntro = [SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN] * 4
+hiHatPhrase.addNoteList(hiHatPitchesIntro, hiHatDurationsIntro)
 
 # hi-hat pattern
-hiHatPitches   = [CHH] * 64                  # Patrón de hihat para versos
-hiHatDurations = [SN] * 64
+hiHatPitches   = [CHH] * 16                  # Patrón de hihat para versos
+hiHatDurations = [SN] * 16
 hiHatPhrase.addNoteList(hiHatPitches, hiHatDurations)
 
-# bass line pattern (follows the kick drum for now)
-bassLinePitches   = [B1, B1] * 8
-bassLineDurations = [SN, SN] * 8
-bassPhrase.addNoteList(bassLinePitches, bassLineDurations)
+# Synth line pattern intro
+SynthPitchesIntro   = [F2, REST, REST, F2, REST, REST, F2, REST, REST, F2, REST, REST, F2, REST, 44 ,REST] * 4  # Patrón del synth para intro y outro
+SynthDurationsIntro = [SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN, SN] * 4
+bassPhrase.addNoteList(SynthPitchesIntro, SynthDurationsIntro)
 
-# bass line pattern 2
-bassLinePitches2   = [G1, G1] * 8
-bassLineDurations2 = [SN, SN] * 8
-bassPhrase.addNoteList(bassLinePitches2, bassLineDurations2) 
+# Synth line pattern verse
+SynthPitches1 = [F2, F2, F2, F2, F2, F2, 44, 44] * 4         # Patrón del synth para versos
+SynthDurations1 = [SN, SN, SN, SN, SN, SN, SN, SN] * 4
+bassPhrase.addNoteList(SynthPitches1, SynthDurations1) 
 
-bassLinePitches3   = [E1, E1] * 8
-bassLineDurations3 = [SN, SN] * 8
-bassPhrase.addNoteList(bassLinePitches3, bassLineDurations3) 
-
-bassLinePitches5 = [C2, C2, C2, C2, C2, C2, B1, B1] * 8
-bassLineDurations5 = [SN, SN, SN, SN, SN, SN, SN, SN] * 8
-bassPhrase.addNoteList(bassLinePitches5, bassLineDurations5) 
+SynthPitches2 = [G2, G2, G2, G2, G2, G2, 46, 46] * 4
+SynthDurations2 = [SN, SN, SN, SN, SN, SN, SN, SN] * 4
+bassPhrase.addNoteList(SynthPitches2, SynthDurations2) 
 
 ##### repeat material as needed
 Mod.repeat(bassDrumPhrase, repetitions)
